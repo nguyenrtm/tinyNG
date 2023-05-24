9113709eee60246ed5a60e72a64e612977735ffa
Nguyen Binh Nguyen <61158419+nguyenrtm@users.noreply.github.com>
2023-05-24 23:29:50 +0700

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,1 @@
-python main.py -c config/config.json \
--o cache/output.txt \
--w cache/model_best_loss.pth \
--p A \
--l 100 \
--m 2
+python main.py -c config/config.json -o cache/output.txt -w cache/model_best_loss.pth -p A -l 100 -m 2
